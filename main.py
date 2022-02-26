d711a48dc63b8c50dbfdb0868131d449b3600112
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-02-26 17:12:06 +0100

    explicaciones y ordenar

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
 import string;
 
-acentos = ["á", "é", "í", "ó", "ú"]
-sinacentos = [ "a", "e", "i", "o", "u"]
-
-
 #la frase que vamos a analizar
 frase = input()
 
+#esta funcion quita todos los espacios, pasa todas las letras a minusculas y quita los acentos
 def hazcadena (lista, cadena):
     if (len(lista)) != 0:
         car = lista.pop()
         cadena = hazcadena(lista,cadena)
         car = car.lower()
         if car != " ":
-            cadena = cadena + car
-    return cadena;
+            cadena = cadena + sustituir_acentos(car)
+    return cadena
+
+#esta es la funcion que quita los acentos, que se usa en la funcion de arriba
+def sustituir_acentos(car):
+    if (car == "á"):
+        return "a"
+    elif (car == "é"):
+        return "e"
+    elif (car == "í"):
+        return "i"
+    elif (car == "ó"):
+        return "o"
+    elif (car == "ú"):
+        return "u"
+    else: 
+        return car
 
 
-#definicion de las funciones que necesitamos
-def quitar_espacios(frase):
+#esta es la funcion principal que prepara la frase inicial para analizar
+def preparar_frase(frase):
     frase_tabla = list(frase)
     cadena = ""
     cadena = hazcadena(frase_tabla, cadena)
     print(cadena)
-    
     return cadena
 
 
-#def sustituir_acentos(frase)
+preparar_frase(frase)
 
-#def todo_mayus(frase)
-
-
-
-quitar_espacios(frase)
-
-#cadena = hazCadena(lista)
 
 
 
