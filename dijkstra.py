4553b26aed84cf2667c319fdda0630d7aee03588
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-02-27 14:44:23 +0100

    funcion ordenar colores y fin

--- dijkstra.py
+++ dijkstra.py
@@ -14,7 +14,24 @@ def crear_bandera():
             bandera.append("A")
     return bandera
 
-
+#funcion que ordena los colores de la bandera
+def ordenar_colores(tamaño, indice, contador):
+    if contador < tamaño:
+        if bandera[indice] == "R":
+            rojo = bandera.pop(indice)
+            bandera.insert(0, rojo)
+            ordenar_colores(tamaño, indice + 1, contador + 1)
+        elif bandera[indice] == "A":
+            azul = bandera.pop(indice)
+            bandera.insert(len(bandera), azul)
+            ordenar_colores(tamaño, indice, contador + 1)
+        else:
+            ordenar_colores(tamaño, indice + 1, contador + 1)
+    else:
+        return bandera
+        
 # aqui empieza el codigo principal
 bandera = crear_bandera()
+print(bandera)
+ordenar_colores(len(bandera), 0, 0)
 print(bandera)
