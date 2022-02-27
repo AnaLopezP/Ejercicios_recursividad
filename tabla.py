d65f8eff361960928bed0de42af74579d564e7d6
Ana López <34817139+AnaLopezP@users.noreply.github.com>
2022-02-27 20:04:00 +0100

    funcion clientes madrid y codigo final

--- tabla.py
+++ tabla.py
@@ -1,14 +1,31 @@
 
-clientes = [["Pepa", "25012", "25"], ["Lucio", "28400", "48"], ["Maria", "45009", "16"], ["Cristopher", "33006","24"], ["Felix", "28010", "43"], ["Noemi", "45200", "18"], ["Miriam", "36066", "36"]]
+clientes = [["Pepa", "25012", "25"], ["Lucio", "33006", "24"], ["Miriam", "45009", "16"], ["Cristopher", "28400","48"], ["Felix", "28010", "43"], ["Noemi", "45200", "18"], ["Maria", "36066", "36"]]
 persona = []
 
 #funcion que mira que clientes son de Toledo por su codigo postal
 def clientes_toledo(tabla, n ,cod_postal):
     if 0 <= n < len(tabla):
-        if tabla[n][1].startwith(cod_postal):
+        if cod_postal in tabla[n][1]:
             persona.append(tabla[n][0])
             clientes_toledo(tabla, n + 1, cod_postal)
         else:
             clientes_toledo(tabla, n + 1, cod_postal)
         return persona
 
+#funcion que mira que clientes entre los 40 y los 50 años son de Madrid
+def clientes_madrid(tabla, n, cod_postal, edad_i, edad_f):
+     if 0 <= n < len(tabla):
+        if cod_postal in tabla[n][1]:
+            if edad_i <= tabla[n][2] <= edad_f:
+                persona.append(tabla[n][0])
+                clientes_madrid(tabla, n + 1, cod_postal, edad_i, edad_f)
+        else:
+            clientes_madrid(tabla, n + 1, cod_postal, edad_i, edad_f)
+        return persona
+
+#codigo principal
+clientes_toledo(clientes, 0, "45")
+print(persona)
+persona = []
+clientes_madrid(clientes, 0, "28", "40", "50")
+print(persona)
